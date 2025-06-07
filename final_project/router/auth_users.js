168430476846aa3176b15e7dd357caf43c6b0495
xDAGAx13 <157238871+xDAGAx13@users.noreply.github.com>
2025-06-07 21:38:57 +0530

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -4,20 +4,49 @@ let books = require("./booksdb.js");
 const regd_users = express.Router();
 
 let users = [];
+const secret = 'yourSecretKey'
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+    let validUsers = users.filter((user)=>{
+        return (user.username===username && user.password ===password)
+    })
+
+    if(validUsers.length>0){
+        return true;
+    }else{
+        return false;
+    }
 }
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+regd_users.post("customer/login", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    // Check if username or password is missing
+    if (!username || !password) {
+        return res.status(404).json({ message: "Error logging in" });
+    }
+    // Authenticate user
+    if (authenticatedUser(username, password)) {
+        // Generate JWT access token
+        let accessToken = jwt.sign({
+            data: password
+        }, 'access', { expiresIn: 60 * 60 });
+        // Store access token and username in session
+        req.session.authorization = {
+            accessToken, username
+        }
+        return res.status(200).send("User successfully logged in");
+    } else {
+        return res.status(208).json({ message: "Invalid Login. Check username and password" });
+    }
 });
+
+
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
